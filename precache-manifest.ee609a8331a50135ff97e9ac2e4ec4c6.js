8447f2ddc4b7d27e8e34d2173a0084b075e2d3b5
rifqieka <ekarifqi@ymail.com>
2019-04-09 10:54:10 +0700

    Updates

---
+++ precache-manifest.ee609a8331a50135ff97e9ac2e4ec4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "896e060f3c0b6ec0b6ce",
+    "url": "/my-app/static/js/main.ab2983d6.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "896e060f3c0b6ec0b6ce",
+    "url": "/my-app/static/css/main.a8b4f298.chunk.css"
+  },
+  {
+    "revision": "a78cf9da116d343282b8ccade961feca",
+    "url": "/my-app/index.html"
+  }
+];
